d1719c412735df1e5d92219ecc89b7aa545dec69
mlum23 <mlum23@my.bcit.ca>
2021-07-23 17:55:12 -0700

    Changed margin for close button in modal

--- corvum/src/components/Graph.js
+++ corvum/src/components/Graph.js
@@ -1,4 +1,4 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Graph = ({ graphInfo, province, dataKey, compareProvince }) => {
 
     const getProvinceInfo = (province) => {
@@ -20,35 +20,34 @@ const Graph = ({ graphInfo, province, dataKey, compareProvince }) => {
                     stroke="#8B0000"
                     dot={false}
                 />
-            )
-
+            );
         }
-
     }
 
     return (
+        <ResponsiveContainer width="100%" height={300}>
+            <LineChart
+                width={600}
+                height={300}
+                margin={{ top: 5, right: 20, bottom: 5, left: 25 }}
+            >
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+                <XAxis dataKey="date" allowDuplicatedCategory={false} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                    name={province}
+                    type="monotone"
+                    data={getProvinceInfo(province)}
+                    dataKey={dataKey}
+                    stroke="#4169E1"
+                    dot={false}
+                />
+                {showCompare()}
 
-        <LineChart
-            width={600}
-            height={300}
-            margin={{ top: 5, right: 20, bottom: 5, left: 25 }}
-        >
-            <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-            <XAxis dataKey="date" allowDuplicatedCategory={false} />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-                name={province}
-                type="monotone"
-                data={getProvinceInfo(province)}
-                dataKey={dataKey}
-                stroke="#4169E1"
-                dot={false}
-            />
-            {showCompare()}
-
-        </LineChart>
+            </LineChart>
+        </ResponsiveContainer>
     )
 }
 
